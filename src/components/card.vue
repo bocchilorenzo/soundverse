12d8f535064497ffdea0de5e6df69a868c30aea4
valefras <48183554+valefras@users.noreply.github.com>
2020-04-27 03:57:15 +0200

    finish

--- src/components/card.vue
+++ src/components/card.vue
@@ -3,17 +3,15 @@
         <v-img
             class="white--text align-end"
             height="200px"
-            src="albumArray.album[n].cover"
+            src="albumArray[n].album.cover"
         >
-            <v-card-title>{{ albumArray.album[n].title }}</v-card-title>
+            <v-card-title>{{ albumArray[n].album.title }}</v-card-title>
         </v-img>
 
         <v-card-subtitle class="pb-0">Number 10</v-card-subtitle>
 
         <v-card-text class="text--primary">
-            <div>Whitehaven Beach</div>
-
-            <div>Whitsunday Island, Whitsunday Islands</div>
+            {{ albumArray[n].album.artist }}
         </v-card-text>
 
         <v-card-actions>
