1b81c7172fe60d957791e97dd33d4e249b9c4206
valefras <48183554+valefras@users.noreply.github.com>
2020-04-30 13:35:10 +0200

    piccoli fix

--- src/views/searchView.vue
+++ src/views/searchView.vue
@@ -1,34 +1,27 @@
 <template>
-    <v-app id="inspire">
-        <v-content>
-            <v-container class="fill-height" fluid>
-                <v-row align="center" justify="center">
-                    <v-col
-                        v-for="album in arrayRisultati"
-                        :key="album.id"
-                        cols="12"
-                        sm="3"
-                        lg="2"
-                        xl="2"
-                    >
-                        <router-link
-                            :to="{
-                                name: 'album',
-                                path: '/album/:id',
-                                params: { id: album.albumId },
-                                props: true,
-                            }"
-                        >
-                            <albumCard
-                                :albumArray="album"
-                                :id="album.albumId"
-                            />
-                        </router-link>
-                    </v-col>
-                </v-row>
-            </v-container>
-        </v-content>
-    </v-app>
+    <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+            <v-col
+                v-for="album in arrayRisultati"
+                :key="album.id"
+                cols="12"
+                sm="3"
+                lg="2"
+                xl="2"
+            >
+                <router-link
+                    :to="{
+                        name: 'album',
+                        path: '/album/:id',
+                        params: { id: album.albumId },
+                        props: true,
+                    }"
+                >
+                    <albumCard :albumArray="album" :id="album.albumId" />
+                </router-link>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <script>
