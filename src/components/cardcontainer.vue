55da467173950fdf9b19f69b74484ab16d5c46b3
bocchilorenzo <lorenzobocchi99@yahoo.com>
2020-06-12 20:28:39 +0200

    Inserito l'username alla registrazione e altro
    Inserito username durante la registrazione e inserito metodo per rimuovere album dal cardContainer

--- src/components/cardcontainer.vue
+++ src/components/cardcontainer.vue
@@ -1,14 +1,7 @@
 <template>
     <v-row align="center">
-        <v-col
-            v-for="album in arrayRisultati"
-            :key="album.albumId"
-            cols="12"
-            sm="4"
-            lg="2"
-            xl="2"
-        >
-            <v-row justify="center">
+        <v-col v-for="album in arrayAlbum" :key="album.albumId" cols="12" sm="4" lg="2" xl="2">
+            <v-row justify="center" >
                 <albumCard :albumArray="album" :id="album.albumId" />
             </v-row>
         </v-col>
@@ -21,7 +14,24 @@ import albumCard from './card'
 export default {
     name: 'cardContainer',
     props: { arrayRisultati: Array },
+    data() {
+        return { arrayAlbum: this.arrayRisultati }
+    },
     components: { albumCard },
+    methods: {
+        //metodo da usare per rimuovere l'album dall'array
+        rimuoviElemento(id) {
+            var i = 0
+            while (i < this.arrayAlbum.length) {
+                if (this.arrayAlbum[i].albumId == id) {
+                    this.arrayAlbum.splice(i, i + 1)
+                    i = 0
+                } else {
+                    i++
+                }
+            }
+        },
+    },
 }
 </script>
 
--- src/views/signup.vue
+++ src/views/signup.vue
@@ -3,12 +3,8 @@
         <h1 style="text-align:center">Registrazione</h1>
         <v-row align="center" justify="center">
             <v-form ref="form">
-                <v-text-field
-                    v-model="email"
-                    :rules="emailRules"
-                    label="E-mail"
-                    required
-                ></v-text-field>
+                <v-text-field v-model="email" :rules="emailRules" label="E-mail" required></v-text-field>
+                <v-text-field v-model="username" label="Username" required></v-text-field>
                 <v-text-field
                     type="password"
                     v-model="password"
@@ -16,7 +12,7 @@
                     label="Password"
                     required
                 ></v-text-field>
-                <v-btn @click="signUpFirebase()">Registrati</v-btn>
+                <v-btn @click="modUsername()">Registrati</v-btn>
             </v-form>
         </v-row>
         <v-row align="center" justify="center" class="mt-3">
@@ -37,6 +33,7 @@ export default {
             email: '',
             password: '',
             password2: '',
+            username: '',
             passRules: [v => !!v || 'Password obbligatoria'],
             emailRules: [
                 v => !!v || 'E-mail obbligatoria',
@@ -55,11 +52,57 @@ export default {
                     .auth()
                     .createUserWithEmailAndPassword(this.email, this.password)
                 alert('Account creato!')
-                this.$router.replace({ name: 'home' })
                 this.$store.commit('updateUserFB')
+                var db = firebase.firestore()
+                var usr = this.username
+                var user = this.$store.state.user
+                var email = user.email
+                var userData = db.collection('utenti').doc(email)
+                userData
+                    .set({
+                        username: usr,
+                    })
+                    .catch(function(error) {
+                        alert('Qualcosa è andato storto, riprova')
+                        console.log(error)
+                    })
+                this.$router.replace({ name: 'home' })
                 //this.$emit('updateUser', firebase.auth().currentUser)
             } catch (err) {
                 alert('Oops. ' + err.message)
+            }
+        },
+        async modUsername() {
+            //Metodo che controlla se l'username è vuoto o meno e se è già presente nel db
+            if (this.username == '' || this.username[0] == ' ') {
+                alert('Il campo username non deve essere vuoto')
+            } else {
+                var db = firebase.firestore()
+                var usr = this.username
+                var userData = db.collection('utenti')
+                var isPresent = false
+                userData
+                    .get()
+                    .then(function(querySnapshot) {
+                        querySnapshot.forEach(function(doc) {
+                            // doc.data() is never undefined for query doc snapshots
+                            if (doc.data().username == usr) {
+                                isPresent = true
+                            }
+                        })
+                    })
+                    .catch(function(error) {
+                        console.log('Error getting document:', error)
+                    })
+                    .then(() => this.setUsername(isPresent))
+            }
+        },
+        setUsername(isPresent) {
+            //Metodo che se l'username è in uso non lo setta, altrimenti lo setta
+            if (isPresent) {
+                alert('Username già in uso')
+            } else {
+                this.signUpFirebase()
             }
         },
     },
